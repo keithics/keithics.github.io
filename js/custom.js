15b7ea81bba6fe5127c977428bb607b2b14164fb
Keith Levi Lumanog <keith@webninjamobile.com>
2017-01-29 15:23:01 +0800

    freelance.ph

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+        var s = 'moc.elibomajninbew@htiek';
+        var e = s.split("").reverse().join("");
+        $('#email').html('<a href="mailto:'+e+'">'+e+'</a>');
+});
